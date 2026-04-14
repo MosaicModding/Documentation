b1b3f406e30c01ffcf40590f84d02097450e71dc
Uraneptus <uraneptus33@gmail.com>
2026-04-14 13:18:17 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Mosaic Modding Documentation',
   tagline: 'We like modding',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico', //TODO this isn't correct on prod
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
